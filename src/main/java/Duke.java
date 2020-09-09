bd6befe2635d18cc116f0383051c60138e42dbcb
TanLeeWei <tanleewei18@gmail.com>
2020-09-10 02:56:00 +0800

    Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -26,62 +26,74 @@ public class Duke {
         do {
             commands = in.nextLine();
             String splitString[] = commands.split(" ", 2);
-
-            switch (splitString[0]) {
-                case "list":
-                    System.out.println(line);
-                    System.out.println("Here are the tasks in your list:");
-                    for(int i = 0; i < numberOfTexts; i++) {
-                        System.out.println(i + 1 + "." + texts[i]);
-                    }
-                    System.out.println("\n" + line);
-                    break;
-                case "todo":
-                    Todo todo = new Todo(splitString[1]);
-                    texts[numberOfTexts] = todo;
-                    numberOfTexts++;
-                    System.out.println(line);
-                    System.out.println("Got it. I've added this task");
-                    System.out.println(todo);
-                    System.out.println("Now you have " + numberOfTexts + " tasks in the list.");
-                    System.out.println("\n" + line);
-                    break;
-                case "deadline":
-                    Deadline deadline = new Deadline(splitString[1]);
-                    deadline.setBy(splitString[1]);
-                    texts[numberOfTexts] = deadline;
-                    numberOfTexts++;
-                    System.out.println(line);
-                    System.out.println("Got it. I've added this task");
-                    System.out.println(deadline);
-                    System.out.println("Now you have " + numberOfTexts + " tasks in the list.");
-                    System.out.println("\n" + line);
-                    break;
-                case "event":
-                    Event event = new Event(splitString[1]);
-                    event.setWhen(splitString[1]);
-                    texts[numberOfTexts] = event;
-                    numberOfTexts++;
-                    System.out.println(line);
-                    System.out.println("Got it. I've added this task");
-                    System.out.println(event);
-                    System.out.println("Now you have " + numberOfTexts + " tasks in the list.");
-                    System.out.println("\n" + line);
-                    break;
-                case "done":
-                    textNumber = Integer.parseInt(splitString[1]) - 1;
-                    texts[textNumber].markAsDone();
-                    System.out.println(line);
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(texts[textNumber]);
-                    System.out.println("\n" + line);
-                    break;
-                case "bye":
-                    break;
-                default:
-                    System.out.println("Invalid command");
-                    break;
+            try {
+                switch (splitString[0]) {
+                    case "list":
+                        System.out.println(line);
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < numberOfTexts; i++) {
+                            System.out.println(i + 1 + "." + texts[i]);
+                        }
+                        System.out.println("\n" + line);
+                        break;
+                    case "todo":
+                        Todo todo = new Todo(splitString[1]);
+                        if(splitString[1].isEmpty()){
+                            throw new ArrayIndexOutOfBoundsException();
+                        }
+                        texts[numberOfTexts] = todo;
+                        numberOfTexts++;
+                        System.out.println(line);
+                        System.out.println("Got it. I've added this task");
+                        System.out.println(todo);
+                        System.out.println("Now you have " + numberOfTexts + " tasks in the list.");
+                        System.out.println("\n" + line);
+                        break;
+                    case "deadline":
+                        Deadline deadline = new Deadline(splitString[1]);
+                        deadline.setBy(splitString[1]);
+                        texts[numberOfTexts] = deadline;
+                        numberOfTexts++;
+                        System.out.println(line);
+                        System.out.println("Got it. I've added this task");
+                        System.out.println(deadline);
+                        System.out.println("Now you have " + numberOfTexts + " tasks in the list.");
+                        System.out.println("\n" + line);
+                        break;
+                    case "event":
+                        Event event = new Event(splitString[1]);
+                        event.setWhen(splitString[1]);
+                        texts[numberOfTexts] = event;
+                        numberOfTexts++;
+                        System.out.println(line);
+                        System.out.println("Got it. I've added this task");
+                        System.out.println(event);
+                        System.out.println("Now you have " + numberOfTexts + " tasks in the list.");
+                        System.out.println("\n" + line);
+                        break;
+                    case "done":
+                        textNumber = Integer.parseInt(splitString[1]) - 1;
+                        texts[textNumber].markAsDone();
+                        System.out.println(line);
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println(texts[textNumber]);
+                        System.out.println("\n" + line);
+                        break;
+                    case "bye":
+                        break;
+                    default:
+                        throw new InvalidCommandException();
+                }
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                System.out.println(line);
+                System.out.println("OOPS!!! The description of a " + splitString[0] + " cannot be empty.");
+                System.out.println("\n" + line);
+            } catch (InvalidCommandException exception) {
+                System.out.println(line);
+                System.out.println("OPPS!!! I'm sorry, but I don;t know what that means :-(");
+                System.out.println("\n" + line);
             }
+
         } while (!commands.equals("bye"));
 
         System.out.println(line);
@@ -89,3 +101,5 @@ public class Duke {
         System.out.println("\n" + line);
     }
 }
+
+
